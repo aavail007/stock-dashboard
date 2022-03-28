d1c446542379371c72623f07d6976029beb29b48
limeng <limeng.chen@flowring.com>
2022-03-28 17:51:27 +0800

    chore:導入 redux + 初步取得 API log

---
+++ src/services/homeServices.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+type TaiwanStockAnalysis = {
+    msg: string,
+    status: number,
+    data: any
+};
+
+export const homeApi = createApi({
+    reducerPath: "homeApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "https://api.web.finmindtrade.com/v2/" }),
+    endpoints: (builder) => ({
+        getTaiwanStockAnalysis: builder.query<TaiwanStockAnalysis, number>({
+            query: (id) => {
+                return `taiwan_stock_analysis?stock_id=${id}`;
+            },
+        })
+    }),
+});
+
+export const { useGetTaiwanStockAnalysisQuery } = homeApi
